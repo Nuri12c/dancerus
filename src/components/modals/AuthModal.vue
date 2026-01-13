26da45ea290a3ae6f0b7363b933d258638657afa
Nuri12c <nurislam.ganieiev@mail.ru>
2026-01-13 12:07:40 +0300

    все ок тут

--- src/components/modals/AuthModal.vue
+++ src/components/modals/AuthModal.vue
@@ -1,47 +1,21 @@
 <template>
-  <div class="modal-overlay" @click.self="close">
+  <div class="modal-overlay" @click.self="$emit('close')">
     <div class="modal-window">
       <div class="modal-content">
         <div class="modal-text-container">
           <h2 class="modal-title">Авторизация</h2>
-
-          <button class="button-1" @click="openLogin">
-            Войти
-          </button>
-
-          <button class="button-1" @click="openRegister">
-            Регистрация
-          </button>
+          <button class="button-1" @click="$emit('open-login')">Войти</button>
+          <button class="button-1" @click="$emit('open-register')">Регистрация</button>
         </div>
       </div>
-
-      <button class="modal-close" @click="close">
-        ✖
-      </button>
+      <button class="modal-close" @click="$emit('close')">✖</button>
     </div>
   </div>
 </template>
 
-<script setup>
-import { useAuthStore } from '@/stores/auth'
-
-const authStore = useAuthStore()
-
-// Закрытие модального окна
-const close = () => {
-  authStore.closeAuthChoice()
-}
-
-// Переход к форме входа
-const openLogin = () => {
-  authStore.closeAuthChoice()
-  authStore.openLogin()
-}
-
-// Переход к форме регистрации
-const openRegister = () => {
-  authStore.closeAuthChoice()
-  authStore.openRegister()
+<script>
+export default {
+  emits: ['open-login', 'open-register', 'close'],
 }
 </script>
 <style scoped lang="scss">
